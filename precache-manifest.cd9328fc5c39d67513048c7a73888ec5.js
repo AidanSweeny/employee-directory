0cf116ece644e5c7297a16088fdc9b7ff806d1f7
Aidan Sweeny <apswee22@colby.edu>
2020-08-14 18:23:17 -0400

    Updates

---
+++ precache-manifest.cd9328fc5c39d67513048c7a73888ec5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa878cb03b76be7aad59999327684d1",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "52acdd04f902bd3dfb3d",
+    "url": "/employee-directory/static/js/2.8e506604.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/employee-directory/static/js/2.8e506604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dffb919b68a693062975",
+    "url": "/employee-directory/static/js/main.dc1b3d15.chunk.js"
+  },
+  {
+    "revision": "c894ba0984b4c9889d74",
+    "url": "/employee-directory/static/js/runtime-main.30ca52ab.js"
+  },
+  {
+    "revision": "3f1073e7b44d16ea08c807649b318f00",
+    "url": "/employee-directory/static/media/ash.3f1073e7.jpg"
+  },
+  {
+    "revision": "7f6660f1ea46797ddfbfd361c810b85c",
+    "url": "/employee-directory/static/media/charizard.7f6660f1.jpg"
+  },
+  {
+    "revision": "18beaeca706c4044f3acb9ae9d248073",
+    "url": "/employee-directory/static/media/misty.18beaeca.jpg"
+  }
+]);
